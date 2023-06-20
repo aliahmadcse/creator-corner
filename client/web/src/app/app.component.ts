d8094ce1ea132eb7bdc44bb3c1c8fef79665319c
Ali Ahmad <aliahmadcse@gmail.com>
2023-06-20 16:29:10 +0500

    modules creation

--- client/web/src/app/app.component.ts
+++ client/web/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  Subscription
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy, OnInit {
   title = 'web';
+  subscription = new Subscription();
+
+  constructor(private activeRouteService: ActivatedRoute) {}
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
