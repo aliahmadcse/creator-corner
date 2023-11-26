cc4d6c4e8b90c44129cad3e91305fc68e6c55bfc
Ali Ahmad <aliahmadcse@gmail.com>
2023-11-26 20:23:07 +0500

    updates the spring boot to 3.2.0

--- api/src/main/java/codes/aliahmad/creatorcorner/CreatorCornerApplication.java
+++ api/src/main/java/codes/aliahmad/creatorcorner/CreatorCornerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CreatorcornerApplication
+public class CreatorCornerApplication
 {
 
   public static void main(String[] args)
   {
-    SpringApplication.run(CreatorcornerApplication.class, args);
+    SpringApplication.run(CreatorCornerApplication.class, args);
   }
 
 }
